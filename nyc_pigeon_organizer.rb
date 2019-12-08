e65df545ebe7fcaa9e9f0211217d12a33215be2d
Aaron Kim <akaaronkim@gmail.com>
2019-12-08 23:36:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+pigeon_data.each do |property, property_hash|
+  property_hash.each do |values, birds_array|
+    birds_array.each do |name|
+      if pigeon_list[name]
+        if pigeon_list[name][property]
+          pigeon_list[name][property] << values
+        else
+          pigeon_list[name][property] = values
+        end
+      else
+        pigeon_list[name] = {property => [values]}
+      end
+    end
+  end
 end
