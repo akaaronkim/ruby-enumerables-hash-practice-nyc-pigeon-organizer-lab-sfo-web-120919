e2dcdd0889741aa59fa0813f211eadae2be014d8
Aaron Kim <akaaronkim@gmail.com>
2019-12-09 00:18:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,3 @@
 def nyc_pigeon_organizer(data)
-  arranged_pigeon_hash = {}
-
-  pigeon_data.each do |attribute, value_hash|
-    value_hash.each do |traits, names_array|
-      names_array.each do |name|
-        if arranged_pigeon_hash[name] ||= { attribute => [traits] }
-          if arranged_pigeon_hash[name][attribute] ||= traits
-            arranged_pigeon_hash[name][attribute] << traits
-          end
-        end
-      end
-    end
-  end
+  
+end
